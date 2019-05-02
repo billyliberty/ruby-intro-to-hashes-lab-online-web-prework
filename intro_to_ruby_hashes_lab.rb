a0ed68754ca0f7af814ecb9796f40dd7d6bcd17f
Joshua Crum <joshua.crum@gmail.com>
2019-05-02 15:07:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,3 +11,16 @@ def monopoly
 		:railroads => {}
 	}
 end
+
+def monopoly_with_second_tier
+	monopoly = {
+		:railroads =>
+		{
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {}
+		}
+	}
+end
+
+
